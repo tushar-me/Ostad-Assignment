3fed16a61baab1161d2d13fe1327c78269f1a05c
TusharImran-Dev <tusharimran.dev@gmail.com>
2023-04-04 21:42:53 +0600

    Assignment-9

---
+++ module9-assignment/contact.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <section>
+        <div class="container">
+            <nav>
+                <div class="nav_logo">
+                    <h1>Blog <span>Website</span> </h1>
+                </div>
+                <div class="nav_links">
+                    <a href="index.php">Home</a>
+                    <a href="blog.php">Blog</a>
+                    <a href="contact.php">Contact Us</a>
+                </div>
+            </nav>
+        </div>
+    </section>
+    <section class="header">
+        <div class="container">
+            <h1>Contact</h1>
+        </div>
+    </section>
+    <section id="contact">
+		<div class="container">
+			<div class="contact_content">
+				<h2>contact us</h2>
+				<div class="contact">
+					<div class="contact_form">
+						<form action="">
+							<div class="name">
+								<p>Full Name</p>
+								<input type="text" name="name" id="name" placeholder="Your name..">
+							</div>
+							<div class="email">
+								<p>Email</p>
+								<input type="email" name="email" id="email" placeholder="Your email..">
+							</div>
+							<div class="massage">
+								<p>Massage</p>
+								<textarea name="massage" id="massage">Write something..</textarea>
+							</div>
+							<input type="submit" value="Submit" class="submit">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ module9-assignment/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Website</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <section>
+        <div class="container">
+            <nav>
+                <div class="nav_logo">
+                    <h1>Blog <span>Website</span> </h1>
+                </div>
+                <div class="nav_links">
+                    <a href="index.php">Home</a>
+                    <a href="blog.php">Blog</a>
+                    <a href="contact.php">Contact Us</a>
+                </div>
+            </nav>
+        </div>
+    </section>
+    <section class="header">
+        <div class="container">
+            <h1>Blog Website</h1>
+        </div>
+    </section>
+    <section class="post_section">
+        <div class="container">
+            <h1 class="text-center p-5">Featured Post</h1>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="post_card">
+                        <img src="assets/images/post1.jpg" alt="">
+                        <span>03/04/2023</span>
+                        <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid reprehenderit debitis, consectetur dolore architecto sed.</p>
+                        <a href="post1.html">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                <div class="post_card">
+                        <img src="assets/images/post2.jpg" alt="">
+                        <span>03/04/2023</span>
+                        <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid reprehenderit debitis, consectetur dolore architecto sed.</p>
+                        <a href="post2.html">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                <div class="post_card">
+                        <img src="assets/images/post3.jpg" alt="">
+                        <span>03/04/2023</span>
+                        <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid reprehenderit debitis, consectetur dolore architecto sed.</p>
+                        <a href="post3.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="contact">
+                <span>Blog Website</span>
+                <a href="contact.php">Contact Us</a>
+            </div>
+        </div>
+    </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
