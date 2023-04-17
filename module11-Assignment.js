4ab0b6dd2967eba76a415bd9163223aa2d05b88c
TusharImran-Dev <tusharimran.dev@gmail.com>
2023-04-17 10:26:19 +0600

    Assignment-11

---
+++ module11-Assignment.js
@@ -0,0 +1,20 @@
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+      let product = num * i;
+    console.log(num + " x " + i + " = " + product);
+    }
+}
+multiplicationTable(5);
+
+/* Output
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+*/
